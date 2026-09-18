cfac237b0f808db939350efed5fa4f371e4b65df
CarlSalomes-dev <carlsalomes@gmail.com>
2026-09-18 12:53:29 +0800

    LE 2.12 Multiply the Digits

--- src/Exercise212/mainExercise212.java
+++ src/Exercise212/mainExercise212.java
@@ -1,10 +1,33 @@
 package Exercise212;
 
+import java.util.Scanner;
+
 public class mainExercise212 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter a number between 0 and 1000: ");
+        int number = input.nextInt();
+
+        int originalNumber = number;
+
+        int digit1 = number / 100;
+        number = number % 100;
+
+        int digit2 = number / 10;
+        int digit3 = number % 10;
+
+        System.out.println(digit1);
+        System.out.println(digit2);
+        System.out.println(digit3);
+
+        int product = digit1 * digit2 * digit3;
+
+        System.out.println("The product of all digits in "
+                + originalNumber + " is " + product);
+
+        input.close();
+    }
 }
